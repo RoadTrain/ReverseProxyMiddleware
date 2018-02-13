d80db99c9484d323bb6298683a053b04158242c3
RoadTrain <pashtet92@bk.ru>
2018-02-13 20:34:59 +0300

    Add information to AssemblyInfo.cs

--- src/ReverseProxyMiddleware/Properties/AssemblyInfo.cs
+++ src/ReverseProxyMiddleware/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Andre Carlucci, ported by RoadTrain")]
 [assembly: AssemblyProduct("ReverseProxyMiddleware")]
 [assembly: AssemblyTrademark("")]
 
@@ -17,3 +17,9 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("9058d441-59f9-43ba-a84a-72e51f78cfe7")]
+[assembly: AssemblyTitle("ReverseProxyMiddleware")]
+[assembly: AssemblyDescription("Reverse Proxy Middleware for OWIN in .NET v4.6")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyCopyright("")]
+
